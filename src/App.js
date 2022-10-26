ba7045f10e560cf2ca8709020459181e3763a241
Raju Raj Kishore <rajsingh9sept@gmail.com>
2022-10-26 14:16:02 +0530

    Add files to the repository

---
+++ src/App.js
@@ -0,0 +1,69 @@
+import "./App.css";
+import Navbar from "./components/Navbar";
+import FormText from "./components/FormText";
+import { useState } from "react";
+import React from 'react';
+
+// import About from "./components/About";
+
+
+
+
+function App() {
+
+  //Below code is JSX react gives you the flexibilty to write all your code in a single file
+  
+  const [mode, setMode] = useState('light'); //this will tell the state of the mode
+
+  const toggleMode = () => {
+    console.log("Switch for Dark Mode clicked");
+    if(mode === 'light') {
+
+      setMode('dark');
+      document.body.style.backgroundColor = '#6F7378';
+    }
+    else{ 
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+    }
+  }
+  
+  return (
+    //This <> empty tag can add all the html in the js file
+    /* <h1>This is the line that i wanted to include</h1>
+    <div classNameName="App">
+    <header classNameName="App-header">
+    <img src={logo} classNameName="App-logo" alt="logo" />
+    <p>
+          Edit <code>src/App.js</code> and save to reload.
+          </p> 
+        <a
+        classNameName="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+        >
+        Learn React with Raju
+        </a>
+        </header>
+      </div> */
+    <>
+    {/* <Navbar title="RajuTextUtils" aboutText="About Me"/> */}
+    <Navbar title="RajuTextUtils" aboutText="About me" mode= {mode} toggleMode = {toggleMode}/>
+
+      <div className="container">
+
+        <FormText textBoxName = "My Text Box" mode = {mode}/>
+      </div>
+      {/* <div className="container">
+        <About/>
+      </div> */}
+
+    </>
+    
+  );
+}
+
+
+
+export default App;
---
+++ src/components/FormText.js
@@ -0,0 +1,77 @@
+import React, { useState } from "react";
+import PropTypes from 'prop-types';
+import { jsPDF } from 'jspdf';
+
+
+export default function FormText(props) {
+
+  const handleUpClick = () => {
+    console.log("UpperCase Button clicked");
+    const newText = text.toUpperCase();
+    setText(newText);
+  }
+
+  const handleOnChange = (event) => {
+    console.log("Handling on change event");
+    setText(event.target.value); //This will listen the event and change the value accordingly
+  }
+
+  const handleLoClick = () => {
+    console.log("Lower case button clicked");
+    const newText = text.toLowerCase();
+    setText(newText); 
+  }
+
+  const handleSavePdf = () => {
+
+    console.log("Saved As PDF");
+    const content  = text;
+    const pdf = new jsPDF();
+    pdf.html(content, {
+      callback: function (doc) {
+          doc.save('sample.pdf');
+      }
+    });
+
+  }
+
+
+  const [text,setText] = useState("Enter text here");
+  return (
+    <>
+    <div>
+      <div className="mb-3">
+        <label htmlFor="myTextBox" className={`form-label text-${props.mode === 'light' ? 'dark' : 'light'}`}>
+          {props.textBoxName}
+        </label>
+        <textarea
+          className={`form-control  text-${props.mode === 'light'?'dark':'light'}`}
+          style={{backgroundColor:props.mode ==='light'?'white':'grey'}}
+          id="myTextBox"
+          rows="5"
+          value={text}
+          onChange = {handleOnChange}
+          placeholder = "Text Box"
+        ></textarea>
+      </div>
+      <button className="btn btn-primary" onClick={handleUpClick}>Convert to Uppercase</button>
+      <button className="btn btn-secondary mx-2" onClick={handleLoClick}>Convert to Lowercase</button>
+      <button className="btn btn-success mx-2" onClick={handleSavePdf}>Save as PDF</button>
+    </div>
+    <div className={`container my-3 text-${props.mode === 'light' ? 'dark' : 'light'}`} >
+      <h2>Words and Characters</h2>
+      <p>Total no of words {text.split(" ").length} and Characters {text.length}</p>
+      <h3>Preview</h3>
+      <p>{text}</p>
+    </div>
+    </>
+  );
+}
+
+FormText.propTypes = {
+    textBoxName : PropTypes.string,
+}
+
+FormText.defaultProps = {
+    textBoxName : "Box name here"
+}
